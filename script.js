1840260b3dce40fe0376929aa3119b7fc159196f
Ulrich Chedjou <ulrich.chedjou@gmail.com>
2024-03-08 13:27:33 +0100

    mon portfolio personnel

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    strings: [ "Frontend Developper" , "Web Developper" , "Backend Developper" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
